a93e05469abe8b9991eefd9900154b728c6ca7c2
cboy <cboy@cboy-8core.local>
2012-10-19 22:01:40 +0900

    Initial Commit

---
+++ AmbientPianoMacOS/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AmbientPianoMacOS
+//
+//  Created by cboy on 12/10/19.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
